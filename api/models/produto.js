9956bade78fbc7fa3fdd3f1fd350a9e6538a44e0
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-12 18:22:43 -0300

    ajustes

--- api/models/produto.js
+++ api/models/produto.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const produtoSchema = new mongoose.Schema({
   nome: String,
   fotos: [String],
-  descrição:String,
+  descricao:String,
   tamanho: String,
   cor: String,
   categoria: { type: mongoose.Schema.Types.ObjectId, ref: 'Categoria' },
