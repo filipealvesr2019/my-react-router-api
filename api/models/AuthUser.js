67277f65e1019484e826c451fe17fda256321284
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-21 11:49:13 -0300

    ajustes

--- api/models/AuthUser.js
+++ api/models/AuthUser.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Digite um email válido!'],
     unique: true,
     lowercase: true,
+    validate: [isEmail, 'Digite um email válido']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 10,
+    required: [true, 'Digite uma senha válida!'],
+    maxLength: [10, 'Digite uma senha com o mínimo de 10 caracteres']
   },
   role: {
     type: String,
-    enum: ['administrador', 'funcionario'],
-    required: true,
-  },
+    required: [true, 'Digite uma credencial válida!'],
+    validate: {
+      validator: validateRole,
+      message: 'Digite uma credencial válida!'
+    }
+  }
 });
+
+function validateRole(value) {
+  const allowedRoles = ['administrador', 'funcionario'];
+  return allowedRoles.includes(value);
+}
 
 const User = mongoose.model('User', userSchema);
 
