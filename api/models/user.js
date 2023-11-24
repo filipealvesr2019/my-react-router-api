64112d8104776ecbf17ae6621318b4f5faf6398e
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-24 08:20:43 -0300

    How to Create User Model Api for ecommerce

---
+++ api/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "Digite o seu nome."],
+        maxLength:[30, "Seu nome não pode exceder 30 caracteres"]
+    },
+    email:{
+        type:String,
+        required:[true, "Digite o seu email."],
+        unique:true,
+        validate:[validator.isEmail, "Digite um endereço de email valido"]
+
+    }, 
+    password:{
+        type:String,
+        required: [true, "Digite uma senha"],
+        minLegth:[10, "Digite uma senha de minimo 10 caracteres"],
+        selected:false
+    },
+    avatar:{
+        public_id:String,
+        required: true
+    },
+    url:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:"UserRole"
+    },
+    create:{
+        type:Date,
+        default:Date.now,
+
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+})
+
+module.exports = mongoose.model("UserRole", userSchema)
