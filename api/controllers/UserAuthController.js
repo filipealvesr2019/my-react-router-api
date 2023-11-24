84c2cca77e71e5f8b2d56c7c12e37f41a15763a8
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-24 12:31:30 -0300

    ajustes

--- api/controllers/UserAuthController.js
+++ api/controllers/UserAuthController.js
@@ -7,13 +7,14 @@ exports.registerUser = async (res, req, next) => {
 
     const user = await User.create({
         name, email, password, avatar:{
-            publica_id: "",
-            url:"michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih"
+            publica_id: "avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih",
+            url:"https://res.cloudinary.com/dcodt2el6/image/upload/v1700826137/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih.jpg"
         }
     })
 
     res.status(201).json({
          success:true,
+         user
          
     })
 }
