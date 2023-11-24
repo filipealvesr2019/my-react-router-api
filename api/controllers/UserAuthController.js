d001ffee5545888c6106a91442e4af3b3a4e6306
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-24 17:42:07 -0300

    ajustes

--- api/controllers/UserAuthController.js
+++ api/controllers/UserAuthController.js
@@ -11,16 +11,41 @@ exports.registerUser = async (req, res, next) => {
             error:"A senha deve ter pelo menos 10 caracteres."
         })
     }
-    const user = await User.create({
-        name, email, password, avatar:{
-            publica_id: "/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih",
-            url:"https://res.cloudinary.com/dcodt2el6/image/upload/v1700826137/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih.jpg"
-        }
-    })
 
-    res.status(201).json({
-         success:true,
-         user
-         
-    })
+    if(!name || name.length === 0){
+        return res.status(400).json({
+            success:false,
+            error:"Digite um nome válido."
+        })
+    }
+
+    if(!email || validator.isEmail(email)){
+        return res.status(400).json({
+            success:false,
+            error:"Digite um endereço de email válido."
+        })
+    }
+
+    try{
+        const user = await User.create({
+            name, email, password, avatar:{
+                publica_id: "/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih",
+                url:"https://res.cloudinary.com/dcodt2el6/image/upload/v1700826137/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih.jpg"
+            }
+        })
+    
+        res.status(201).json({
+             success:true,
+             user
+             
+        })
+    }catch(error){
+        console.error("Erro, ao cadastrar usuario" ,error);
+        res.status(500).json({
+            success:false,
+            error:"Erro interno do servidor."
+        })
+    }
+
+ 
 }
