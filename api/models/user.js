6a7b46d276415fc32bbe0d3b2f89c6db575b9f34
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-24 09:42:59 -0300

    cm

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+const validator = require("validator")
 
 
 const userSchema = new mongoose.Schema({
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "Digite uma senha"],
-        minlength: [10, "Digite uma senha de no mínimo 10 caracteres"],
+        minLength: [10, "Digite uma senha de no mínimo 10 caracteres"],
         select: false
     },
     
