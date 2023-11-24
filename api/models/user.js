7bcac89192149967d3a9934698ab80ae93a1d1db
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-24 12:04:37 -0300

    cm

--- api/models/user.js
+++ api/models/user.js
@@ -1,46 +1,45 @@
 const mongoose = require("mongoose");
-const validator = require("validator")
-
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, "Digite o seu nome."],
-        maxLength:[30, "Seu nome não pode exceder 30 caracteres"]
+    name: {
+        type: String,
+        required: [true, "Digite o seu nome."],
+        maxLength: [30, "Seu nome não pode exceder 30 caracteres"],
     },
-    email:{
-        type:String,
-        required:[true, "Digite o seu email."],
-        unique:true,
-        validate:[validator.isEmail, "Digite um endereço de email valido"]
-
-    }, 
+    email: {
+        type: String,
+        required: [true, "Digite o seu email."],
+        unique: true,
+        validate: [validator.isEmail, "Digite um endereço de email válido"],
+    },
     password: {
         type: String,
         required: [true, "Digite uma senha"],
         minLength: [10, "Digite uma senha de no mínimo 10 caracteres"],
-        select: false
+        select: false,
     },
-    
-    avatar:{
-        public_id:String,
-        required: true
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        required: [true, "Avatar is required"],
     },
-    url:{
-        typeStr:String,
-        required:true
+    url: {
+        type: String,
+        required: [true, "URL is required"],
     },
-    role:{
-        type:String,
-        default:"UserRole"
+    role: {
+        type: String,
+        default: "UserRole",
     },
-    create:{
-        type:Date,
-        default:Date.now,
+    create: {
+        type: Date,
+        default: Date.now,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+});
 
-    },
-    resetPasswordToken:String,
-    resetPasswordExpire:Date
-})
-
-module.exports = mongoose.model("UserRole", userSchema)
+module.exports = mongoose.model("UserRole", userSchema);
