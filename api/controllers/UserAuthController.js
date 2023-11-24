748d08c1901f69e9ff876fc9aa8742226cd0ba87
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-24 17:29:23 -0300

    ajsutes

--- api/controllers/UserAuthController.js
+++ api/controllers/UserAuthController.js
@@ -4,7 +4,13 @@ const User = require("../models/user")
 // cadastro de usuarios => /api/v1/register
 exports.registerUser = async (req, res, next) => {
     const { name, email, password} = req.body;
-
+    
+    if(password.length < 10){
+        return res.status(400).json({
+            success:false,
+            error:"A senha deve ter pelo menos 10 caracteres."
+        })
+    }
     const user = await User.create({
         name, email, password, avatar:{
             publica_id: "/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih",
