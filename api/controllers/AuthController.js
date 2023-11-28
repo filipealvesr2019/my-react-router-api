16bc060b85654dd2320c37cd068a90e9bae957a7
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-28 19:43:10 -0300

    ajustes

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,59 +1,55 @@
 // controllers/AuthController.js
-const User = require('../models/AuthUser');
-const jwt = require('jsonwebtoken');
+const User = require("../models/AuthUser");
+const jwt = require("jsonwebtoken");
 const validator = require("validator");
 const sendToken = require("../utils/jwtToken");
 
 // cadastro de usuarios => /api/v1/register
 const registerUser = async (req, res, next) => {
-  const { email, password, role} = req.body;
-  
-  if(password.length < 10){
-      return res.status(400).json({
-          success:false,
-          error:"A senha deve ter pelo menos 10 caracteres."
-      })
+  const { email, password, role } = req.body;
+
+  if (password.length < 10) {
+    return res.status(400).json({
+      success: false,
+      error: "A senha deve ter pelo menos 10 caracteres.",
+    });
   }
-
-
 
   if (!email || !validator.isEmail(email)) {
-      return res.status(400).json({
-          success: false,
-          error: "Digite um endereço de email válido.",
-      });
+    return res.status(400).json({
+      success: false,
+      error: "Digite um endereço de email válido.",
+    });
   }
 
-  try{
-      const user = await User.create({
-          email, password, role ,  avatar:{
-              publica_id: "/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih",
-              url:"https://res.cloudinary.com/dcodt2el6/image/upload/v1700826137/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih.jpg"
-          }
-      })
-      
-      
-  sendToken(user, 200, res)
-  }catch(error){
-      console.error("Erro, ao cadastrar usuario" ,error);
-      res.status(500).json({
-          success:false,
-          error:"Erro interno do servidor."
-      })
+  try {
+    const user = await User.create({
+      email,
+      password,
+      role,
+      avatar: {
+        publica_id: "/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih",
+        url: "https://res.cloudinary.com/dcodt2el6/image/upload/v1700826137/avatars/michael-dam-mEZ3PoFGs_k-unsplash_2_pmcmih.jpg",
+      },
+    });
+
+    sendToken(user, 200, res);
+  } catch (error) {
+    console.error("Erro, ao cadastrar usuario", error);
+    res.status(500).json({
+      success: false,
+      error: "Erro interno do servidor.",
+    });
   }
-
-
-}
+};
 
 // logar usuario com JWT token
 const loginUser = async (req, res, next) => {
   const { email, password } = req.body;
 
-
-
   // verifica se o usuario esta logado com email e senha
   if (!email || !password) {
-    console.log('Email ou senha ausentes');
+    console.log("Email ou senha ausentes");
     return res.status(400).json({
       success: false,
       error: "Email e senha são obrigatórios.",
@@ -64,7 +60,7 @@ const loginUser = async (req, res, next) => {
   const user = await User.findOne({ email }).select("+password +role");
 
   if (!user) {
-    console.log('Usuário não encontrado');
+    console.log("Usuário não encontrado");
     return res.status(401).json({
       success: false,
       error: "Email ou senha inválidos.",
@@ -75,7 +71,7 @@ const loginUser = async (req, res, next) => {
   const isPasswordMatch = user.comparePassword(password);
 
   if (!isPasswordMatch) {
-    console.log('Senha incorreta');
+    console.log("Senha incorreta");
     return res.status(401).json({
       success: false,
       error: "Email ou senha inválidos.",
@@ -85,31 +81,28 @@ const loginUser = async (req, res, next) => {
   // Agora, dependendo do papel (role) do usuário, você pode realizar ações específicas
   if (user.role === "administrador") {
     // Lógica para administrador
-
     // Adicione aqui as ações específicas para o administrador
   } else if (user.role === "funcionario") {
     // Lógica para funcionário
-
     // Adicione aqui as ações específicas para o funcionário
   }
 
   // Envie o token para o usuário
-  console.log('Enviando token para o usuário');
+  console.log("Enviando token para o usuário");
   sendToken(user, 200, res);
 };
-
 
 const getUser = async (req, res) => {
   try {
     const userId = req.params.id;
     const user = await User.findById(userId).exec();
     if (!user) {
-      return res.status(404).send('Usuário não encontrado!');
+      return res.status(404).send("Usuário não encontrado!");
     }
     res.status(200).json(user);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Erro interno do servidor ao buscar usuário!');
+    res.status(500).send("Erro interno do servidor ao buscar usuário!");
   }
 };
 
@@ -123,12 +116,12 @@ const updateUser = async (req, res) => {
       { new: true }
     );
     if (!updatedUser) {
-      return res.status(404).send('Usuário não encontrado para atualização!');
+      return res.status(404).send("Usuário não encontrado para atualização!");
     }
     res.status(200).json(updatedUser);
   } catch (error) {
     console.error(error);
-    res.status(400).send('Erro ao atualizar usuário!');
+    res.status(400).send("Erro ao atualizar usuário!");
   }
 };
 
@@ -137,12 +130,12 @@ const deleteUser = async (req, res) => {
     const userId = req.params.id;
     const deletedUser = await User.findByIdAndDelete(userId);
     if (!deletedUser) {
-      return res.status(404).send('Usuário não encontrado para exclusão!');
+      return res.status(404).send("Usuário não encontrado para exclusão!");
     }
-    res.status(200).json({ message: 'Usuário excluído com sucesso!' });
+    res.status(200).json({ message: "Usuário excluído com sucesso!" });
   } catch (error) {
     console.error(error);
-    res.status(400).send('Erro ao excluir usuário!');
+    res.status(400).send("Erro ao excluir usuário!");
   }
 };
 
@@ -151,15 +144,14 @@ const getUserByUsername = async (req, res) => {
     const username = req.query.username; // Obtendo o nome de usuário do query parameters
     const user = await User.findOne({ username: username }).exec();
     if (!user) {
-      return res.status(404).send('Usuário não encontrado!');
+      return res.status(404).send("Usuário não encontrado!");
     }
     res.status(200).json(user);
   } catch (error) {
     console.error(error);
-    res.status(400).send('Erro ao buscar usuário especifico!');
+    res.status(400).send("Erro ao buscar usuário especifico!");
   }
 };
-
 
 const getAllUsers = async (req, res) => {
   try {
@@ -167,22 +159,57 @@ const getAllUsers = async (req, res) => {
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
-    res.status(400).send('Erro do servidor ao buscar todos os usuários!');
+    res.status(400).send("Erro do servidor ao buscar todos os usuários!");
   }
 };
 
 // deslogar um usuario api/v1/logout
-const logout = async (req, res, next) =>{
+const logout = async (req, res, next) => {
   res.cookie("token", null, {
-      expires: new Date(Date.now()),
-      httpOnly:true,
-  })
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
   res.status(200).json({
-      success:true,
-      message:"Deslogado"
-  })
-}
+    success: true,
+    message: "Deslogado",
+  });
+};
 
-module.exports = {  loginUser,
-  registerUser, getUser, updateUser, deleteUser, getUserByUsername, getAllUsers, logout  };
+// deslogar um usuario api/v1/logout
+// AuthController.js
+const Userlogout = async (req, res, next) => {
+  try {
+    // Lógica para limpar o cookie com o token
+    res.cookie("token", "", {
+      expires: new Date(0),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    // Resposta com uma mensagem de sucesso
+    res.status(200).json({
+      success: true,
+      message: "Deslogado com sucesso.",
+    });
+  } catch (error) {
+    // Trate qualquer erro que ocorra durante o logout
+    console.error("Erro ao fazer logout:", error);
+    res.status(500).json({
+      success: false,
+      error: "Erro interno do servidor ao fazer logout.",
+    });
+  }
+};
+
+module.exports = {
+  loginUser,
+  Userlogout,
+  registerUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  getUserByUsername,
+  getAllUsers,
+  logout,
+};
--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const protect = require('../middleware/protect');
+const protect = require("../middleware/protect");
 
 const {
   getUser,
@@ -10,17 +10,18 @@ const {
   getAllUsers,
   loginUser,
   registerUser,
-} = require('../controllers/AuthController');
-const { logout } = require('../controllers/UserAuthController');
-const { isAuthenticatedUser } = require("../middleware/auth")
+} = require("../controllers/AuthController");
+const { Userlogout } = require("../controllers/AuthController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
-router.get('/users', getAllUsers); // Rota para buscar todos os usuários
-router.post('/login',  loginUser); // Use directly from AuthController
-router.post('/user',  registerUser); // Use directly from AuthController
-router.get('/user/:id', getUser); // Rota para buscar usuário por ID
-router.put('/user/:id',isAuthenticatedUser,  updateUser); // Rota para atualizar usuário por ID
-router.delete('/user/:id',isAuthenticatedUser,  deleteUser); // Rota para excluir usuário por ID
-router.get('/user', getUserByUsername); // Rota para buscar usuário por nome de usuário
-router.route("/logout").get(logout);
+router.get("/users", getAllUsers); // Rota para buscar todos os usuários
+router.post("/login", loginUser); // Use directly from AuthController
+
+router.post("/user", registerUser); // Use directly from AuthController
+router.get("/user/:id", getUser); // Rota para buscar usuário por ID
+router.put("/user/:id", isAuthenticatedUser, updateUser); // Rota para atualizar usuário por ID
+router.delete("/user/:id", isAuthenticatedUser, deleteUser); // Rota para excluir usuário por ID
+router.get("/user", getUserByUsername); // Rota para buscar usuário por nome de usuário
+router.route("/logout").post(Userlogout);
 
 module.exports = router;
