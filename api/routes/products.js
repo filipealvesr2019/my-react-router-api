80ff9543607d6b8be103fd81fa226a69d618331f
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-28 19:11:35 -0300

    add

--- api/routes/products.js
+++ api/routes/products.js
@@ -20,8 +20,8 @@ const { isAuthenticatedUser } = require("../middleware/auth")
 router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
 router.route("/admin/product/new").post(newProduct);
-router.route("/admin/product/:id").put(isAuthenticatedUser, protect, adminAuth, updateProduct);
-router.route("/admin/product/:id").delete(isAuthenticatedUser, protect, adminAuth, deleteProduct);
+router.route("/admin/product/:id").put(updateProduct);
+router.route("/admin/product/:id").delete( deleteProduct);
 router.route("/review").put(isAuthenticatedUser, createProductReview);
 router.get("/reviews", isAuthenticatedUser, getProductReviews);
 router.route("/review").delete(isAuthenticatedUser, deleteReview);
