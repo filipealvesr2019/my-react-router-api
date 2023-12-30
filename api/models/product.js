21eeaf626e5111ab3e4286b28602860e12880ce8
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-30 19:12:29 -0300

    add

--- api/models/product.js
+++ api/models/product.js
@@ -1,67 +1,66 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Entre o nome do Produto"],
-    trim: true,
-    maxLength: [100, "O Produto não pode exceder 100 palavras"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Digite o preço"],
-    maxLength: [5, "O preço não pode exceder 5 números"],
-    default: 0.0,
-  },
-  description: {
-    type: String,
-    required: [true, "Digite a descrição"],
-  },
-  images: [
-    {
-      colors: [
-        {
-          color: {
-            type: String,
-            required: true,
-          },
-          url: {
-            type: String,
-            required: true,
-          },
-        },
-      ],
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Entre o nome do Produto"],
+      trim: true,
+      maxLength: [100, "O Produto não pode exceder 100 palavras"],
     },
-  ],
-  size: {
-    type: String,
-    required: [true, "Digite o tamanho do produto"],
-  },
-  category: {
-    type: String, // Mudança: altere para String
-    required: [true, "Digite a categoria do produto"],
-  },
-  subcategory: {
-    type: String, // Mudança: altere para String
-  },
-  inStock: {
-    type: Boolean,
-    default: true,
-  },
-  quantity: {
-    type: Number,
-    default: 1,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  lastModifiedAt: {
-    type: Date,
-    default: Date.now,
-  },
+    price: {
+      type: Number,
+      required: [true, "Digite o preço"],
+      maxLength: [5, "O preço não pode exceder 5 números"],
+      default: 0.0,
+    },
+    description: {
+      type: String,
+      required: [true, "Digite a descrição"],
+    },
 
-  
-}, { timestamps: true });
+    colors: [
+      {
+        color: {
+          type: String,
+          required: true,
+        },
+        urls: {
+          type: [String],
+          required: true,
+        },
+      },
+    ],
+    
+    size: {
+      type: String,
+      required: [true, "Digite o tamanho do produto"],
+    },
+    category: {
+      type: String, // Mudança: altere para String
+      required: [true, "Digite a categoria do produto"],
+    },
+    subcategory: {
+      type: String, // Mudança: altere para String
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    lastModifiedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);
