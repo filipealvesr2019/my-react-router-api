4bea133a658e4980ddf31206fd646e909c4dae7c
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-01 10:20:22 -0300

    add

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,10 +1,9 @@
 const Product = require("../models/product");
-const axios = require('axios');  // Certifique-se de que o caminho do modelo está correto
-
+const axios = require("axios"); // Certifique-se de que o caminho do modelo está correto
 
 const APIFeatures = require("../utils/APIFeatures");
-const multer = require('multer');
-  // Importe a biblioteca Cloudinary
+const multer = require("multer");
+// Importe a biblioteca Cloudinary
 // Configuração do Cloudinary
 
 // Configuração do Multer para upload de imagens em memória
@@ -14,15 +13,18 @@ const upload = multer({ storage: storage });
 // Função para fazer o upload da imagem para o ImgBB
 const uploadImageToImgBB = async (imageBuffer) => {
   try {
-    const response = await axios.post('https://api.imgbb.com/1/upload', {
+    const response = await axios.post("https://api.imgbb.com/1/upload", {
       key: process.env.ImgBB_KEY, // Substitua com sua chave de API ImgBB
-      image: imageBuffer.toString('base64'),
+      image: imageBuffer.toString("base64"),
     });
 
     // A resposta conterá informações sobre a imagem enviada
     return response.data.data;
   } catch (error) {
-    console.error('Erro ao fazer upload da imagem para o ImgBB:', error.message);
+    console.error(
+      "Erro ao fazer upload da imagem para o ImgBB:",
+      error.message
+    );
     throw error;
   }
 };
@@ -34,18 +36,18 @@ exports.newProduct = async (req, res, next) => {
     const product = new Product(req.body);
 
     // Salva o produto no banco de dados
-    await product.save();
+    await Product.insertMany([product]);
 
-    console.log('Produto salvo no banco de dados.');
+    console.log("Produto salvo no banco de dados.");
     res.status(201).json({
       success: true,
       product,
     });
   } catch (error) {
-    console.error('Erro ao salvar o produto no banco de dados:', error);
+    console.error("Erro ao salvar o produto no banco de dados:", error);
     res.status(500).json({
       success: false,
-      message: 'Erro interno do servidor',
+      message: "Erro interno do servidor",
     });
   }
 };
@@ -53,87 +55,87 @@ exports.newProduct = async (req, res, next) => {
 // mostrar produtos => /api/products
 // mostrar produtos => /api/products
 exports.getProducts = async (req, res, next) => {
-    try {
-        const resPerPage = 8;
-        let productsCount;
+  try {
+    const resPerPage = 8;
+    let productsCount;
 
-        // Verificar se os parâmetros de preço foram fornecidos
-        let priceFilter = {};
-        if (req.query.minPrice && req.query.maxPrice) {
-            priceFilter = {
-                price: {
-                    $gte: req.query.minPrice,
-                    $lte: req.query.maxPrice
-                }
-            };
-        }
-
-        // Contar o número total de produtos considerando os filtros
-        productsCount = await Product.countDocuments(priceFilter);
-
-        // Consultar produtos com os filtros aplicados
-        const apiFeatures = new APIFeatures(Product.find(priceFilter), req.query)
-            .search()
-            .filter()
-            .pagination(resPerPage);
-
-        const products = await apiFeatures.query;
-
-        res.status(200).json({
-            success: true,
-            productsCount,
-            resPerPage,
-            products
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message
-        });
+    // Verificar se os parâmetros de preço foram fornecidos
+    let priceFilter = {};
+    if (req.query.minPrice && req.query.maxPrice) {
+      priceFilter = {
+        price: {
+          $gte: req.query.minPrice,
+          $lte: req.query.maxPrice,
+        },
+      };
     }
+
+    // Contar o número total de produtos considerando os filtros
+    productsCount = await Product.countDocuments(priceFilter);
+
+    // Consultar produtos com os filtros aplicados
+    const apiFeatures = new APIFeatures(Product.find(priceFilter), req.query)
+      .search()
+      .filter()
+      .pagination(resPerPage);
+
+    const products = await apiFeatures.query;
+
+    res.status(200).json({
+      success: true,
+      productsCount,
+      resPerPage,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 // mostrar produtos => /api/products
 // mostrar produtos => /api/products
 exports.getProducts = async (req, res, next) => {
-    try {
-        const resPerPage = 8;
-        let productsCount;
+  try {
+    const resPerPage = 10;
+    let productsCount;
 
-        // Verificar se os parâmetros de preço foram fornecidos
-        let priceFilter = {};
-        if (req.query.minPrice && req.query.maxPrice) {
-            priceFilter = {
-                price: {
-                    $gte: req.query.minPrice,
-                    $lte: req.query.maxPrice
-                }
-            };
-        }
-
-        // Contar o número total de produtos considerando os filtros
-        productsCount = await Product.countDocuments(priceFilter);
-
-        // Consultar produtos com os filtros aplicados
-        const apiFeatures = new APIFeatures(Product.find(priceFilter), req.query)
-            .search()
-            .filter()
-            .pagination(resPerPage);
-
-        const products = await apiFeatures.query;
-
-        res.status(200).json({
-            success: true,
-            productsCount,
-            resPerPage,
-            products
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message
-        });
+    // Verificar se os parâmetros de preço foram fornecidos
+    let priceFilter = {};
+    if (req.query.minPrice && req.query.maxPrice) {
+      priceFilter = {
+        price: {
+          $gte: req.query.minPrice,
+          $lte: req.query.maxPrice,
+        },
+      };
     }
+
+    // Contar o número total de produtos considerando os filtros
+    productsCount = await Product.countDocuments(priceFilter);
+
+    // Consultar produtos com os filtros aplicados
+    const apiFeatures = new APIFeatures(Product.find(priceFilter), req.query)
+      .search()
+      .filter()
+      .pagination(resPerPage);
+
+    const products = await apiFeatures.query;
+
+    res.status(200).json({
+      success: true,
+      productsCount,
+      resPerPage,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 // mostrar produto especifico por id => /api/v1/product/:id
@@ -159,15 +161,16 @@ exports.updateProduct = async (req, res) => {
     const updatedProduct = req.body;
 
     // Use mongoose to find and update the product
-    const result = await Product.findByIdAndUpdate(productId, updatedProduct, { new: true });
+    const result = await Product.findByIdAndUpdate(productId, updatedProduct, {
+      new: true,
+    });
 
     res.json(result);
   } catch (error) {
-    console.error('Error updating product:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
-
 
 // deletar produtos => /api/v1/admin/product/:id
 exports.deleteProduct = async (req, res, next) => {
@@ -197,92 +200,80 @@ exports.deleteProduct = async (req, res, next) => {
   }
 };
 
-
 // criar/atualisar novo review => /api/review
 exports.createProductReview = async (req, res, next) => {
-
   const { rating, comment, productId } = req.body;
 
   const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
 
-    user:req.user._id,
-    name:req.user.name,
-    rating:Number(rating),
-    comment
-  }
-
-  const product =  await Product.findById(productId);
-  if(!product){
+  const product = await Product.findById(productId);
+  if (!product) {
     return res.status(404).json({
-      success:false,
-      error:"id do Produto não existe"
-    })
+      success: false,
+      error: "id do Produto não existe",
+    });
   }
   const isReviewed =
-  Array.isArray(product.reviews) &&
-  product.reviews.find(
-    (r) => r && r.user && r.user.toString() === req.user._id.toString()
-  );
-  if(isReviewed){
-    product.reviews.forEach(review => {
-      if(review.user.toString() === req.user._id.toString()){
+    Array.isArray(product.reviews) &&
+    product.reviews.find(
+      (r) => r && r.user && r.user.toString() === req.user._id.toString()
+    );
+  if (isReviewed) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
         review.Comment = comment;
         review.rating = rating;
       }
-    })
-
+    });
   } else {
     product.reviews.push(review);
-    product.numOfReviews = product.reviews.length
+    product.numOfReviews = product.reviews.length;
   }
 
-  product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product;
-  review.length
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) / product;
+  review.length;
 
   await product.save({
-    validateBeforeSave:false
-  })
+    validateBeforeSave: false,
+  });
 
   res.status(200).json({
-    success:true,
-    message:"review enviado com sucesso"
-  })
-
-}
-
-
+    success: true,
+    message: "review enviado com sucesso",
+  });
+};
 
 // mostrar lista de reviews /api/reviews
 // mostrar lista de reviews /api/reviews
-exports.getProductReviews = async (req, res, next) =>{
+exports.getProductReviews = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.query.id);
 
-  
-  try{
-    const product =  await Product.findById(req.query.id)
-    
-    if(!product){
+    if (!product) {
       return res.status(404).json({
-        success:false,
-        error:"Erro produto nao encontrado com esse id."
-
-      })
+        success: false,
+        error: "Erro produto nao encontrado com esse id.",
+      });
     }
 
-  res.status(200).json({
-    success:true,
-    reviews: product.reviews || [],
-  })
-
-  }
-  catch(error){
-    console.error("Erro ao obter avaliações do produto: ",error);
+    res.status(200).json({
+      success: true,
+      reviews: product.reviews || [],
+    });
+  } catch (error) {
+    console.error("Erro ao obter avaliações do produto: ", error);
     res.status(500).json({
-      success:false,
-      error:"Erro interno do servidor ao obter avaliações do produto."
-    })
-
+      success: false,
+      error: "Erro interno do servidor ao obter avaliações do produto.",
+    });
   }
-}
+};
 exports.deleteReview = async (req, res, next) => {
   try {
     const productId = req.query.id; // Captura o 'id' da consulta
@@ -305,19 +296,10 @@ exports.deleteReview = async (req, res, next) => {
     console.error("Erro ao excluir avaliação do produto: ", error);
     res.status(500).json({
       success: false,
-      error:
-        "Erro interno do servidor ao excluir avaliação do produto.",
+      error: "Erro interno do servidor ao excluir avaliação do produto.",
     });
   }
 };
-
-
-
-
-
-
-
-
 
 // Rota para obter todos os produtos de uma categoria específica por nome
 exports.getProductsByCategory = async (req, res) => {
@@ -326,12 +308,17 @@ exports.getProductsByCategory = async (req, res) => {
     const products = await Product.find({ category: categoryName });
 
     if (!products || products.length === 0) {
-      return res.status(404).json({ success: false, message: 'Nenhum produto encontrado para esta categoria' });
+      return res
+        .status(404)
+        .json({
+          success: false,
+          message: "Nenhum produto encontrado para esta categoria",
+        });
     }
 
     res.status(200).json({ success: true, products });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, error: 'Erro interno do servidor' });
+    res.status(500).json({ success: false, error: "Erro interno do servidor" });
   }
 };
