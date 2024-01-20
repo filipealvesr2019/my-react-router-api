2ba080e84fe32c91aa53c266e046ff7a1a57b731
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-20 14:30:36 -0300

    add

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middleware/protect");
-const adminAuth = require("../middleware/adminAuth");
-const { getUser, updateUser, deleteUser, getUserByUsername, getAllUsers, loginUser, registerUser } = require("../controllers/AuthController");
+
+const {
+  getUser,
+  updateUser,
+  deleteUser,
+  getUserByUsername,
+  getAllUsers,
+  loginUser,
+  registerUser,
+} = require("../controllers/AuthController");
 const { Userlogout } = require("../controllers/AuthController");
+const { isAuthenticatedUser } = require("../middleware/auth");
+const auth = require("../middleware/auth");
 
-// Apply the protect middleware to all routes below this point
-router.use(protect);
-
-// Apply adminAuth middleware to restrict access to admin-only routes
-router.use(adminAuth);
-
-// All routes below this point require both authentication and admin authorization
-
-router.get("/users", protect, getAllUsers); // Rota para buscar todos os usuários
+router.get("/users", getAllUsers, auth, isAuthenticatedUser); // Rota para buscar todos os usuários
 router.post("/login", loginUser); // Use directly from AuthController
 
 router.post("/user", registerUser); // Use directly from AuthController
@@ -21,6 +22,6 @@ router.get("/user/:id", getUser); // Rota para buscar usuário por ID
 router.put("/user/:id", updateUser); // Rota para atualizar usuário por ID
 router.delete("/user/:id", deleteUser); // Rota para excluir usuário por ID
 router.get("/user", getUserByUsername); // Rota para buscar usuário por nome de usuário
-router.route("/logout").post(Userlogout);
+router.route("/logout").post(auth, Userlogout);
 
 module.exports = router;
