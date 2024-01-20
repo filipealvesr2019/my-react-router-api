61f4f80ca3b4859b31bad0005d8628bc77b0738e
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-20 14:31:15 -0300

    add

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -11,10 +11,8 @@ const {
   registerUser,
 } = require("../controllers/AuthController");
 const { Userlogout } = require("../controllers/AuthController");
-const { isAuthenticatedUser } = require("../middleware/auth");
-const auth = require("../middleware/auth");
 
-router.get("/users", getAllUsers, auth, isAuthenticatedUser); // Rota para buscar todos os usuários
+router.get("/users", getAllUsers); // Rota para buscar todos os usuários
 router.post("/login", loginUser); // Use directly from AuthController
 
 router.post("/user", registerUser); // Use directly from AuthController
@@ -22,6 +20,6 @@ router.get("/user/:id", getUser); // Rota para buscar usuário por ID
 router.put("/user/:id", updateUser); // Rota para atualizar usuário por ID
 router.delete("/user/:id", deleteUser); // Rota para excluir usuário por ID
 router.get("/user", getUserByUsername); // Rota para buscar usuário por nome de usuário
-router.route("/logout").post(auth, Userlogout);
+router.route("/logout").post( Userlogout);
 
 module.exports = router;
