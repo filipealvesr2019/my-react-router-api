430f24d1eaa15a1531dd54efe2359a619d911565
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-20 14:48:22 -0300

    add

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken'); // Adicione esta linha
-const { isAuthenticatedUser } = require("../middleware/auth")
 
 const {
   getUser,
@@ -14,53 +12,14 @@ const {
 } = require("../controllers/AuthController");
 const { Userlogout } = require("../controllers/AuthController");
 
-// Middleware de autenticação
-const authenticateUser = (req, res, next) => {
-  // Verifica se há um token JWT no cabeçalho da requisição
-  const token = req.header('Authorization');
-  if (!token) {
-    return res.status(401).json({ message: 'Token não fornecido' });
-  }
+router.get("/users", getAllUsers); // Rota para buscar todos os usuários
+router.post("/login", loginUser); // Use directly from AuthController
 
-  try {
-    // Verifica o token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
-};
-
-// Middleware de autorização para administradores
-const authorizeAdmin = (req, res, next) => {
-  // Verifica se o usuário tem a função de administrador
-  const userId = req.user.id; // Id do usuário obtido do token
-  User.findById(userId, (err, user) => {
-    if (err) {
-      return res.status(500).json({ message: 'Erro ao verificar função de administrador' });
-    }
-
-    if (!user || user.role !== 'administrador') {
-      return res.status(403).json({ message: 'Acesso não autorizado' });
-    }
-
-    next();
-  });
-};
-
-// Rotas protegidas
-router.get("/users", isAuthenticatedUser, authorizeAdmin, getAllUsers);
-router.get("/user/:id", authenticateUser, authorizeAdmin, getUser);
-router.put("/user/:id", authenticateUser, authorizeAdmin, updateUser);
-router.delete("/user/:id", authenticateUser, authorizeAdmin, deleteUser);
-
-// Rotas públicas
-router.post("/login", loginUser);
-router.post("/user", registerUser);
-router.get("/user", getUserByUsername);
-
-// Rota de logout protegida
-router.route("/logout").post(authenticateUser, Userlogout);
+router.post("/user", registerUser); // Use directly from AuthController
+router.get("/user/:id", getUser); // Rota para buscar usuário por ID
+router.put("/user/:id", updateUser); // Rota para atualizar usuário por ID
+router.delete("/user/:id", deleteUser); // Rota para excluir usuário por ID
+router.get("/user", getUserByUsername); // Rota para buscar usuário por nome de usuário
+router.route("/logout").post( Userlogout);
 
 module.exports = router;
