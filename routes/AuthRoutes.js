48e896dc59269718683c7fcb9cb449ca46afd4ff
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-21 11:39:35 -0300

    add

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -17,10 +17,10 @@ router.get("/users",isAuthenticatedUser, getAllUsers); // Rota para buscar todos
 router.post("/login", loginUser); // Use directly from AuthController
 
 router.post("/user", registerUser); // Use directly from AuthController
-router.get("/user/:id", getUser); // Rota para buscar usuário por ID
-router.put("/user/:id", updateUser); // Rota para atualizar usuário por ID
-router.delete("/user/:id", deleteUser); // Rota para excluir usuário por ID
-router.get("/user", getUserByUsername); // Rota para buscar usuário por nome de usuário
+router.get("/user/:id",isAuthenticatedUser, getUser); // Rota para buscar usuário por ID
+router.put("/user/:id",isAuthenticatedUser, updateUser); // Rota para atualizar usuário por ID
+router.delete("/user/:id",isAuthenticatedUser, deleteUser); // Rota para excluir usuário por ID
+router.get("/user",isAuthenticatedUser, getUserByUsername); // Rota para buscar usuário por nome de usuário
 router.route("/logout").post( Userlogout);
 
 module.exports = router;
