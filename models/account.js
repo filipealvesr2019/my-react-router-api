3eb7499ff7a32e86e9ee35a4bb5158e3c368e076
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-23 20:52:01 -0300

    add

--- models/account.js
+++ models/account.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
   name: String,
+  price: Number
   // Adicione outros campos conforme necessário para representar uma conta
 });
 
