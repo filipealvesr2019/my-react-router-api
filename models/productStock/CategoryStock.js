0f1570d08fd810f7fae65df90e5634e4254addaa
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-24 11:14:39 -0300

    add

--- models/productStock/CategoryStock.js
+++ models/productStock/CategoryStock.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const categoryStockSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String },
 });
 
 const CategoryStock = mongoose.model('CategoryStock', categoryStockSchema);
