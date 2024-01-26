67e0d48d826da8dee199bc1102923c58fb28f368
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-26 18:41:34 -0300

    add

--- models/vendor.js
+++ models/vendor.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const vendorSchema = new mongoose.Schema({
   name: String,
-  TaxpayerIDNumber: Number,
+  TaxpayerIDNumber: String,
   email: String,
   phoneNumber: Number,
 
