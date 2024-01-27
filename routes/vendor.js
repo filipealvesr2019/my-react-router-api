a8f430c6c2b397c7180dcc2f565f7ea2a4298c65
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-26 21:13:05 -0300

    add

--- routes/vendor.js
+++ routes/vendor.js
@@ -1,11 +1,10 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Vendor = require('../models/vendor'); 
+const Vendor = require("../models/vendor");
 // Rota para obter todos os fornecedores
 
 // Rota para obter todos os fornecedores com paginação
-router.get('/vendor', async (req, res) => {
+router.get("/vendor", async (req, res) => {
   const page = parseInt(req.query.page) || 1; // Número da página (padrão: 1)
   const limit = parseInt(req.query.limit) || 10; // Tamanho da página (padrão: 10)
 
@@ -20,7 +19,7 @@ router.get('/vendor', async (req, res) => {
       vendors,
       page,
       totalPages,
-      totalVendors
+      totalVendors,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -28,71 +27,71 @@ router.get('/vendor', async (req, res) => {
 });
 
 // Rota para criar um novo fornecedor
-router.post('/vendor', async (req, res) => {
+router.post("/vendor", async (req, res) => {
   const vendor = new Vendor({
     name: req.body.name,
     TaxpayerIDNumber: req.body.TaxpayerIDNumber,
     email: req.body.email,
-    phoneNumber: req.body.phoneNumber
+    phoneNumber: req.body.phoneNumber,
     // Adicione outros campos conforme necessário
   });
 
   try {
-    const newVendor= await vendor.save();
+    const newVendor = await vendor.save();
     res.status(201).json(newVendor);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 
-
 // Rota para pesquisar fornecedores por nome
-router.get('/vendor/search', async (req, res) => {
+router.get("/vendor/search", async (req, res) => {
   const searchName = req.query.name;
 
   try {
-    const vendors = await Vendor.find({ name: { $regex: new RegExp(searchName, 'i') } });
+    const vendors = await Vendor.find({
+      name: { $regex: new RegExp(searchName, "i") },
+    });
     res.json(vendors);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-
-
 // Rota para excluir um fornecedor por ID
-router.delete('/vendor/:id', async (req, res) => {
+router.delete("/vendor/:id", async (req, res) => {
   const vendorId = req.params.id;
 
   try {
     const deletedVendor = await Vendor.findByIdAndDelete(vendorId);
 
     if (!deletedVendor) {
-      return res.status(404).json({ message: 'Fornecedor não encontrado.' });
+      return res.status(404).json({ message: "Fornecedor não encontrado." });
     }
 
-    res.json({ message: 'Fornecedor excluído com sucesso.' });
+    res.json({ message: "Fornecedor excluído com sucesso." });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-
-
 // Rota para editar um fornecedor por ID
-router.put('/vendor/:id', async (req, res) => {
+router.put("/vendor/:id", async (req, res) => {
   const vendorId = req.params.id;
 
   try {
     // Verifique se o fornecedor existe
     const existingVendor = await Vendor.findById(vendorId);
     if (!existingVendor) {
-      return res.status(404).json({ message: 'Fornecedor não encontrado.' });
+      return res.status(404).json({ message: "Fornecedor não encontrado." });
     }
 
     // Atualize os campos do fornecedor com base nos dados fornecidos no corpo da solicitação
-    existingVendor.name = req.body.name; // Atualize conforme necessário
-    
+    // Atualize os campos do fornecedor com base nos dados fornecidos no corpo da solicitação
+    existingVendor.name = req.body.name;
+    existingVendor.TaxpayerIDNumber = req.body.TaxpayerIDNumber;
+    existingVendor.email = req.body.email;
+    existingVendor.phoneNumber = req.body.phoneNumber;
     // Salve as alterações no banco de dados
     const updatedVendor = await existingVendor.save();
 
@@ -101,6 +100,5 @@ router.put('/vendor/:id', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
 
 module.exports = router;
