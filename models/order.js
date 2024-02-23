0add280c7b025205133dbd6c673eb8dbcdfaf7e0
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-23 10:09:48 -0300

    add

--- models/order.js
+++ models/order.js
@@ -2,69 +2,22 @@ const mongoose = require("mongoose");
 const { v4: uuidv4 } = require('uuid');
 
 const orderSchema = mongoose.Schema({
-  shoppingInfo: {  // Corrigido de shopingInfo para shoppingInfo
-    country: {
-      type: String,
-      required: [true, "O país é obrigatório."]
-    },
-    postalCode: {
-      type: String,
-      required: [true, "O código postal é obrigatório."]
-    },
-    city: {
-      type: String,
-      required: [true, "A cidade é obrigatória."]
-    },
-    state: {
-      type: String,
-      required: [true, "O estado é obrigatório."]
-    },
-    address: {
-      type: String,
-      required: [true, "O endereço é obrigatório."]
-    }
-  },
+ 
+
   customer: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Customer',
-    required: [true, "O cliente é obrigatório."]
-  },
-  orderItems: [
-    {
-      name: {
-        type: String,
-        required: true
-      },
-      quantity: {
-        type: Number,
-        required: true
-      },
-      image: {
-        type: String,
-        required: true
-      },
-      price: {
-        type: Number,
-        required: true
-      },
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Product"
-      },
-    }
-  ],
-  paymentInfo: {
-    id: {
-      type: String,
-      default: uuidv4(),
+    required: [true, "O cliente é obrigatório."],
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Product"
     },
-    status: {
-      type: String
+    total:{type:Number},
+    quantity: {
+      type: Number,
+      required: true
     }
-  },
-  paidAt: {
-    type: Date
   },
   shippingFee: {
     type: Number,
