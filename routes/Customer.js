0dfe5acc4969b86dd7b61b6733bf7b889c87e1f7
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-28 19:19:48 -0300

    add creditCard payment

--- routes/Customer.js
+++ routes/Customer.js
@@ -9,6 +9,7 @@ const axios = require("axios");
 const Frete = require("../models/Frete");
 const Pix = require("../models/Pix");
 const Boleto = require("../models/Boleto");
+const CreditCard = require("../models/CreditCard");
 // Rota para criar um novo usuário
 
 router.post("/signup", async (req, res) => {
@@ -1004,6 +1005,137 @@ router.post("/boleto/:clerkUserId", async (req, res) => {
       });
 
       await boleto.save();
+    }
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+router.post("/creditCard/:clerkUserId", async (req, res) => {
+  try {
+    const token = process.env.ACCESS_TOKEN;
+    const clerkUserId = req.params.clerkUserId; // Agora é uma string
+
+    // Encontra o cliente associado ao atendente
+    const customer = await Customer.findOne({ clerkUserId: clerkUserId });
+
+    if (!customer) {
+      return res.status(404).json({ message: "Cliente não encontrado." });
+    }
+
+    // Encontra o carrinho do cliente
+    const cart = await Cart.findOne({ customer: customer._id }).populate(
+      "products.productId"
+    );
+
+    if (!cart) {
+      return res.status(404).json({ message: "Carrinho não encontrado." });
+    }
+
+    // Encontra o asaasCustomerId do cliente
+    const asaasCustomerId = customer.asaasCustomerId;
+    const totalPrice = cart.products.reduce(
+      (total, product) => total + product.productId.price * product.quantity,
+      0
+    );
+    const totalAmount = totalPrice + cart.shippingFee;
+
+
+
+
+
+
+    
+    // Cria uma string vazia para armazenar os IDs dos produtos
+    let externalReferences = "";
+
+    // Itera sobre os produtos no carrinho
+    for (const product of cart.products) {
+      // Adiciona o ID do produto à string externalReferences
+      externalReferences += product.productId._id + ",";
+    }
+
+    // Remove a vírgula extra no final da string externalReferences
+    externalReferences = externalReferences.slice(0, -1);
+
+    // Apaga os registros de frete anteriores
+    const data = {
+      billingType: "CREDIT_CARD",
+      discount: { value: 0, dueDateLimitDays: 0 },
+      interest: { value: 0 },
+      fine: { value: 0 },
+      customer: asaasCustomerId, // Substitui 'cus_000005895208' pelo asaasCustomerId
+      dueDate: new Date(), // Define a data atual como a data de vencimento
+      value: totalAmount,
+      description: "Pedido 056984",
+      daysAfterDueDateToCancellationRegistration: 1,
+      externalReference: externalReferences,
+      postalService: false,
+    };
+
+    const response = await axios.post(
+      "https://sandbox.asaas.com/api/v3/payments",
+      data,
+      {
+        headers: {
+          accept: " 'application/json'",
+          "content-type": "application/json",
+          access_token: token,
+        },
+      }
+    );
+
+    // Verifica se a resposta é um array
+    if (Array.isArray(response.data)) {
+      // Se for um array, faz um loop sobre os itens e salva cada um
+      for (const item of response.data) {
+        const creditCard = new CreditCard({
+          clerkUserId: clerkUserId, // Agora é uma string
+          customer: item.customer,
+          billingType: item.billingType,
+          value: item.value,
+          externalReference: item.externalReference,
+          invoiceUrl: item.invoiceUrl,
+          bankSlipUrl: item.bankSlipUrl,
+          dueDate: item.dueDate,
+        });
+
+        await creditCard.save();
+      }
+    } else {
+      // Se não for um array, salva apenas um item
+      const creditCard = new CreditCard({
+        clerkUserId: clerkUserId, // Agora é uma string
+        customer: response.data.customer,
+        billingType: response.data.billingType,
+        value: response.data.value,
+        externalReference: response.data.externalReference,
+        invoiceUrl: response.data.invoiceUrl,
+        bankSlipUrl: response.data.bankSlipUrl,
+        dueDate: response.data.dueDate,
+      });
+
+      await creditCard.save();
     }
 
     res.json(response.data);
