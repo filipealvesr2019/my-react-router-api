a5915300be86a9e484a6fe658ed712d2a88eae26
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-28 18:15:23 -0300

    add

---
+++ models/CreditCard.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const creditCardSchema = new mongoose.Schema({
+  holderName: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
+  expiryMonth: {
+    type: String,
+    required: true
+  },
+  expiryYear: {
+    type: String,
+    required: true
+  },
+  ccv: {
+    type: String,
+    required: true
+  }
+});
+
+const customerSchema = new mongoose.Schema({
+  clerkUserId: {
+    type: String,
+    required: true
+  },
+  customer: {
+    type: String,
+    required: true
+  },
+  billingType: {
+    type: String,
+    enum: ['BOLETO', 'CREDIT_CARD', 'PIX'],
+    required: true
+  },
+  value: {
+    type: Number,
+    required: true
+  },
+  externalReference: {
+    type: String
+  },
+  invoiceUrl: {
+    type: String
+  },
+  bankSlipUrl: {
+    type: String
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  creditCard: creditCardSchema
+});
+
+const CreditCard = mongoose.model('CreditCard', customerSchema);
+
+module.exports = CreditCard;
