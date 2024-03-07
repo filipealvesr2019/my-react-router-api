4c5e3abef3726e79a756c6ccb624efb160bb5601
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-07 20:40:04 -0300

    add

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -28,7 +28,7 @@ const registerUser = async (req, res, next) => {
     const user = await User.create({
       email,
       password,
-      role
+      role,
     });
 
     sendToken(user, 200, res);
@@ -41,11 +41,88 @@ const registerUser = async (req, res, next) => {
   }
 };
 
+// cadastro de usuarios => /api/v1/register
+const registerCustumer = async (req, res, next) => {
+  const { email, password, role } = req.body;
 
+  if (password.length < 10) {
+    return res.status(400).json({
+      success: false,
+      error: "A senha deve ter pelo menos 10 caracteres.",
+    });
+  }
 
+  if (!email || !validator.isEmail(email)) {
+    return res.status(400).json({
+      success: false,
+      error: "Digite um endereço de email válido.",
+    });
+  }
 
+  try {
+    const user = await User.create({
+      email,
+      password,
+      role,
+    });
 
+    sendToken(user, 200, res);
+  } catch (error) {
+    console.error("Erro, ao cadastrar usuario", error);
+    res.status(500).json({
+      success: false,
+      error: "Erro interno do servidor.",
+    });
+  }
+};
 
+// logar usuario com JWT token
+const loginCustumer = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // verifica se o usuario esta logado com email e senha
+  if (!email || !password) {
+    console.log("Email ou senha ausentes");
+    return res.status(400).json({
+      success: false,
+      error: "Email e senha são obrigatórios.",
+    });
+  }
+
+  // procurando usuario no banco de dados
+  const user = await User.findOne({ email }).select("+password +role");
+
+  if (!user) {
+    console.log("Usuário não encontrado");
+    return res.status(401).json({
+      success: false,
+      error: "Email ou senha inválidos.",
+    });
+  }
+
+  // verifica se a senha está correta ou não
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    console.log("Senha incorreta");
+    return res.status(401).json({
+      success: false,
+      error: "Email ou senha inválidos.",
+    });
+  }
+
+  // Agora, dependendo do papel (role) do usuário, você pode realizar ações específicas
+  if (user.role === "customer") {
+    // Lógica para administrador
+    // Adicione aqui as ações específicas para o administrador
+  } else {
+    console.log("Erro: Permissões de usuário não reconhecidas.");
+  }
+
+  // Envie o token para o usuário
+  console.log("Enviando token para o usuário");
+  sendToken(user, 200, res);
+};
 
 // logar usuario com JWT token
 const loginUser = async (req, res, next) => {
@@ -93,19 +170,14 @@ const loginUser = async (req, res, next) => {
     // Lógica para funcionário
     // Adicione aqui as ações específicas para o funcionário
   } else if (user.role === "customer") {
-  // Lógica para funcionário
-  // Adicione aqui as ações específicas para o funcionário
-}
-
-
-  
+    // Lógica para funcionário
+    // Adicione aqui as ações específicas para o funcionário
+  }
 
   // Envie o token para o usuário
   console.log("Enviando token para o usuário");
   sendToken(user, 200, res);
-  
 };
-
 
 const getUser = async (req, res) => {
   try {
@@ -178,7 +250,6 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-
 // deslogar um usuario api/v1/logout
 const logout = async (req, res, next) => {
   res.cookie("token", null, {
@@ -218,13 +289,6 @@ const Userlogout = async (req, res, next) => {
   }
 };
 
-
-
-
-
-
-
-
 // Função para enviar email de recuperação de senha
 const sendPasswordResetEmail = async (req, res) => {
   const { email } = req.body;
@@ -248,15 +312,16 @@ const sendPasswordResetEmail = async (req, res) => {
     // Enviar o email de recuperação de senha usando o Postmark
     const postmarkApiKey = process.env.POSTMARK_API_KEY;
 
-if (!postmarkApiKey) {
-  console.error("Chave do Postmark não encontrada. Configure a variável de ambiente POSTMARK_API_KEY.");
-  return; // Ou faça outro tratamento de erro adequado
-}
+    if (!postmarkApiKey) {
+      console.error(
+        "Chave do Postmark não encontrada. Configure a variável de ambiente POSTMARK_API_KEY."
+      );
+      return; // Ou faça outro tratamento de erro adequado
+    }
 
-const client = new postmark.ServerClient(postmarkApiKey);
+    const client = new postmark.ServerClient(postmarkApiKey);
 
-const resetLink = `http://localhost:5173/reset-password/${resetToken}`;
-
+    const resetLink = `http://localhost:5173/reset-password/${resetToken}`;
 
     await client.sendEmail({
       From: "ceo@mediewal.com.br",
@@ -267,18 +332,18 @@ const resetLink = `http://localhost:5173/reset-password/${resetToken}`;
     <p>Por favor, clique no botão abaixo para redefinir sua senha:</p>
     <a href="${resetLink}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">Redefinir Senha</a>
   `,
-});
+    });
     res.status(200).json({
       message: "Email de recuperação de senha enviado com sucesso.",
     });
   } catch (error) {
     console.error("Erro ao enviar email de recuperação de senha:", error);
     res.status(500).json({
-      message: "Erro interno do servidor ao enviar email de recuperação de senha.",
+      message:
+        "Erro interno do servidor ao enviar email de recuperação de senha.",
     });
   }
 };
-
 
 // Função para redefinir a senha
 const resetPassword = async (req, res) => {
@@ -286,7 +351,9 @@ const resetPassword = async (req, res) => {
 
   // Verificar se todas as informações necessárias foram fornecidas
   if (!token || !newPassword || !confirmPassword) {
-    return res.status(400).json({ message: "Todos os campos são obrigatórios." });
+    return res
+      .status(400)
+      .json({ message: "Todos os campos são obrigatórios." });
   }
 
   try {
@@ -308,7 +375,9 @@ const resetPassword = async (req, res) => {
     res.status(200).json({ message: "Senha redefinida com sucesso." });
   } catch (error) {
     console.error("Erro ao redefinir senha:", error);
-    res.status(500).json({ message: "Erro interno do servidor ao redefinir senha." });
+    res
+      .status(500)
+      .json({ message: "Erro interno do servidor ao redefinir senha." });
   }
 };
 
@@ -323,6 +392,7 @@ module.exports = {
   getAllUsers,
   logout,
   sendPasswordResetEmail,
-  resetPassword
-  
+  resetPassword,
+  registerCustumer,
+  loginCustumer,
 };
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const postmark = require('postmark');
+const validator = require('validator');
+const User = require('../models/AuthUser');
+const bcrypt = require('bcryptjs'); // Para gerar senhas temporárias
+const jwt = require("jsonwebtoken");
 const {
   getUser,
   updateUser,
@@ -8,12 +13,19 @@ const {
   getAllUsers,
   loginUser,
   registerUser,
+  registerCustumer,
+  loginCustumer
 } = require("../controllers/AuthController");
 const { Userlogout } = require("../controllers/AuthController");
 const { isAuthenticated, isAdmin } = require("../middleware/middlewares.authMiddleware");
 const AuthController = require('../controllers/AuthController')
 router.get("/users",isAuthenticated, getAllUsers); // Rota para buscar todos os usuários
 router.post("/login", loginUser); // Use directly from AuthController
+
+router.post("/user",  registerUser); // Use directly from AuthController
+
+router.post("/custumer",  registerCustumer); // Use directly from AuthController
+router.post("/loginCustumer", loginCustumer); // Use directly from AuthController
 
 router.post("/user",  registerUser); // Use directly from AuthController
 router.get("/user/:id", getUser); // Rota para buscar usuário por ID
@@ -48,5 +60,76 @@ router.post("/forgot-password", AuthController.sendPasswordResetEmail);
 router.post('/reset-password/:token',  AuthController.resetPassword);
 
 
+
+
+
+// Função para enviar e-mail usando Postmark
+const sendEmail = async (email, token) => {
+  const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
+
+  try {
+    const registrationLink = `http://localhost:3001/register/${token}`;
+
+    await client.sendEmail({
+      From: "ceo@mediewal.com.br",
+      To: email,
+      Subject: "Link de registro",
+      TextBody: `Clique no seguinte link para se registrar: ${registrationLink}`,
+      HtmlBody: `<p>Clique <a href="${registrationLink}">aqui</a> para se registrar.</p>`,
+    });
+
+    console.log("E-mail enviado com sucesso");
+  } catch (error) {
+    console.error("Erro ao enviar e-mail", error);
+  }
+};
+
+// Rota para solicitar registro
+router.post("/register/request", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // Gerar token JWT com duração de 10 minutos
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "10m" });
+
+    // Enviar e-mail com o link de registro contendo o token
+    await sendEmail(email, token);
+
+    res.status(200).json({ success: true, message: "Link de registro enviado com sucesso." });
+  } catch (error) {
+    console.error("Erro ao solicitar registro", error);
+    res.status(500).json({ success: false, error: "Erro interno do servidor." });
+  }
+});
+
+// Rota para registrar usuário com o token
+router.post("/register/:token", async (req, res) => {
+  const { token } = req.params;
+  const { email, password, role } = req.body;
+
+  try {
+    // Verificar se o token é válido
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Verificar se o e-mail no token corresponde ao fornecido no corpo da solicitação
+    if (decodedToken.email !== email) {
+      return res.status(400).json({ success: false, error: "Token inválido para este e-mail." });
+    }
+
+    // Aqui você pode adicionar mais validações, se necessário
+
+    // Criar usuário com e-mail e senha fornecidos
+    const user = await User.create({
+      email,
+      password,
+      role,
+    });
+
+    res.status(201).json({ user, success: true, message: "Usuário registrado com sucesso." });
+  } catch (error) {
+    console.error("Erro ao registrar usuário", error);
+    res.status(500).json({ success: false, error: "Erro interno do servidor." });
+  }
+});
 
 module.exports = router;
