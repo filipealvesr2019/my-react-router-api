7c650bc0cf8c314197c6fa692aa5ef94ffb3433a
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-24 19:52:38 -0300

    add

--- models/paymentReports.js
+++ models/paymentReports.js
@@ -1,56 +1,227 @@
 const mongoose = require('mongoose');
 
 const paymentReportsSchema = new mongoose.Schema({
-
-
-    dateCreated: Date,
-    customer: String,
-    paymentLink: String,
-    value: Number,
-    netValue: Number,
-    originalValue: Number,
-    interestValue: Number,
-    description: String,
-    billingType: String,
-    canBePaidAfterDueDate: Boolean,
-    pixTransaction: String,
-    status: String,
-    dueDate: Date,
-    originalDueDate: Date,
-    paymentDate: Date,
-    clientPaymentDate: Date,
-    installmentNumber: Number,
-    invoiceUrl: String,
-    invoiceNumber: String,
-    externalReference: String,
-    deleted: Boolean,
-    anticipated: Boolean,
-    anticipable: Boolean,
-    creditDate: Date,
-    estimatedCreditDate: Date,
-    transactionReceiptUrl: String,
-    nossoNumero: String,
-    bankSlipUrl: String,
-    lastInvoiceViewedDate: Date,
-    lastBankSlipViewedDate: Date,
-    discount: {
-      value: Number,
-      limitDate: Date,
-      dueDateLimitDays: Number,
-      type: String
+    event: {
+        type: String,
+        required: true
     },
-    fine: {
-      value: Number,
-      type: String
-    },
-    interest: {
-      value: Number,
-      type: String
-    },
-    postalService: Boolean,
-    custody: String,
-    refunds: [String]
-  });
+    payment: {
+        object: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        },
+        dateCreated: {
+            type: Date,
+            required: true
+        },
+        customer: {
+            type: String,
+            required: true
+        },
+        subscription: {
+            type: String
+        },
+        installment: {
+            type: String
+        },
+        paymentLink: {
+            type: String
+        },
+        dueDate: {
+            type: Date,
+            required: true
+        },
+        originalDueDate: {
+            type: Date,
+            required: true
+        },
+        value: {
+            type: Number,
+            required: true
+        },
+        netValue: {
+            type: Number,
+            required: true
+        },
+        originalValue: {
+            type: Number
+        },
+        interestValue: {
+            type: Number
+        },
+        nossoNumero: {
+            type: String
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        externalReference: {
+            type: String,
+            required: true
+        },
+        billingType: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        pixTransaction: {
+            type: String
+        },
+        confirmedDate: {
+            type: Date,
+            required: true
+        },
+        paymentDate: {
+            type: Date,
+            required: true
+        },
+        clientPaymentDate: {
+            type: Date,
+            required: true
+        },
+        installmentNumber: {
+            type: Number
+        },
+        creditDate: {
+            type: Date,
+            required: true
+        },
+        custody: {
+            type: String
+        },
+        estimatedCreditDate: {
+            type: Date,
+            required: true
+        },
+        invoiceUrl: {
+            type: String
+        },
+        bankSlipUrl: {
+            type: String
+        },
+        transactionReceiptUrl: {
+            type: String
+        },
+        invoiceNumber: {
+            type: String,
+            required: true
+        },
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        anticipated: {
+            type: Boolean,
+            default: false
+        },
+        anticipable: {
+            type: Boolean,
+            default: false
+        },
+        lastInvoiceViewedDate: {
+            type: Date
+        },
+        lastBankSlipViewedDate: {
+            type: Date
+        },
+        postalService: {
+            type: Boolean,
+            default: false
+        },
+        creditCard: {
+            creditCardNumber: {
+                type: String
+            },
+            creditCardBrand: {
+                type: String
+            },
+            creditCardToken: {
+                type: String
+            }
+        },
+        discount: {
+            value: {
+                type: Number,
+                default: 0
+            },
+            dueDateLimitDays: {
+                type: Number,
+                default: 0
+            },
+            limitedDate: {
+                type: Date
+            },
+            type: {
+                type: String,
+                default: 'FIXED'
+            }
+        },
+        fine: {
+            value: {
+                type: Number,
+                default: 0
+            },
+            type: {
+                type: String,
+                default: 'FIXED'
+            }
+        },
+        interest: {
+            value: {
+                type: Number,
+                default: 0
+            },
+            type: {
+                type: String,
+                default: 'PERCENTAGE'
+            }
+        },
+        split: [
+            {
+                walletId: {
+                    type: String,
+                    required: true
+                },
+                fixedValue: {
+                    type: Number
+                },
+                percentualValue: {
+                    type: Number
+                },
+                status: {
+                    type: String,
+                    required: true
+                },
+                refusalReason: {
+                    type: String
+                }
+            }
+        ],
+        chargeback: {
+            status: {
+                type: String,
+                required: true
+            },
+            reason: {
+                type: String,
+                required: true
+            }
+        },
+        refunds: [
+            {
+                // Definir o esquema de reembolso, se aplicável
+            }
+        ]
+    }
+});
 
 const PaymentReports = mongoose.model('PaymentReports', paymentReportsSchema);
 
