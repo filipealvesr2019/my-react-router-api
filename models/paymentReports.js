5d69d0ac3459817aba0c993c8730b1c28ba20ff5
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-08 18:45:56 -0300

    add

--- models/paymentReports.js
+++ models/paymentReports.js
@@ -4,7 +4,41 @@ const mongoose = require('mongoose');
 const paymentReportsSchema = new mongoose.Schema({
   event: String,
   payment:{
-    id: String
+    object: String,
+  id: String,
+  dateCreated: Date,
+  customer: String,
+  paymentLink: String,
+  value: Number,
+  netValue: Number,
+  description: String,
+  billingType: String,
+  canBePaidAfterDueDate: Boolean,
+  status: String,
+  dueDate: Date,
+  originalDueDate: Date,
+  paymentDate: Date,
+  clientPaymentDate: Date,
+  installmentNumber: Number,
+  invoiceUrl: String,
+  invoiceNumber: String,
+  externalReference: String,
+  deleted: Boolean,
+  anticipated: Boolean,
+  anticipable: Boolean,
+  creditDate: Date,
+  estimatedCreditDate: Date,
+  transactionReceiptUrl: String,
+  nossoNumero: String,
+  bankSlipUrl: String,
+  lastInvoiceViewedDate: Date,
+  lastBankSlipViewedDate: Date,
+  discount: discountSchema, // Usando o esquema de desconto definido acima
+  fine: fineSchema, // Usando o esquema de multa definido acima
+  interest: interestSchema, // Usando o esquema de juros definido acima
+  postalService: Boolean,
+  custody: String,
+  refunds: mongoose.Schema.Types.Mixed // Pode conter qualquer tipo de dado
   }
 });
 const PaymentReports = mongoose.model('PaymentReports', paymentReportsSchema);
