8cd6e0ac88d2f2d3afb2f352f2486e2983ead6c8
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-08 18:18:52 -0300

    add

--- models/paymentReports.js
+++ models/paymentReports.js
@@ -2,53 +2,53 @@ const mongoose = require('mongoose');
 
 
 const paymentReportsSchema = new mongoose.Schema({
-    object: { type: String },
-  id: { type: String },
-  dateCreated: { type: Date },
-    customer: { type: String  },
-    paymentLink: { type: String  },
-    value: {type: Number},
-    netValue: Number,
-    description: String,
-    billingType: String,
-    canBePaidAfterDueDate: Boolean,
-    status: String,
-    dueDate: Date,
-    originalDueDate: Date,
-    paymentDate: Date,
-    clientPaymentDate: Date,
-    installmentNumber: Number,
-    invoiceUrl: String,
-    invoiceNumber: String,
-    externalReference: String,
-    deleted: Boolean,
-    anticipated: Boolean,
-    anticipable: Boolean,
-    creditDate: Date,
-    estimatedCreditDate: Date,
-    transactionReceiptUrl: String,
-    nossoNumero: String,
-    bankSlipUrl: String,
-    lastInvoiceViewedDate: Date,
-    lastBankSlipViewedDate: Date,
-    discount: {
-      value: Number,
-      limitDate: Date,
-      dueDateLimitDays: Number,
-      type: String
-    },
-    fine: {
-      value: Number,
-      type: String
-    },
-    interest: {
-      value: Number,
-      type: String
-    },
-    postalService: Boolean,
-    custody: String,
-    refunds: mongoose.Schema.Types.Mixed // Pode conter qualquer tipo de dado
-  });
+  object: String,
+  id: String,
+  dateCreated: Date,
+  customer: String,
+  paymentLink: String,
+  value: Number,
+  netValue: Number,
+  description: String,
+  billingType: String,
+  canBePaidAfterDueDate: Boolean,
+  status: String,
+  dueDate: Date,
+  originalDueDate: Date,
+  paymentDate: Date,
+  clientPaymentDate: Date,
+  installmentNumber: Number,
+  invoiceUrl: String,
+  invoiceNumber: String,
+  externalReference: String,
+  deleted: Boolean,
+  anticipated: Boolean,
+  anticipable: Boolean,
+  creditDate: Date,
+  estimatedCreditDate: Date,
+  transactionReceiptUrl: String,
+  nossoNumero: String,
+  bankSlipUrl: String,
+  lastInvoiceViewedDate: Date,
+  lastBankSlipViewedDate: Date,
+  discount: {
+    value: Number,
+    limitDate: Date,
+    dueDateLimitDays: Number,
+    type: String
+  },
+  fine: {
+    value: Number,
+    type: String
+  },
+  interest: {
+    value: Number,
+    type: String
+  },
+  postalService: Boolean,
+  custody: String,
+  refunds: mongoose.Schema.Types.Mixed // Pode conter qualquer tipo de dado
+});
 const PaymentReports = mongoose.model('PaymentReports', paymentReportsSchema);
 
 module.exports = PaymentReports;
