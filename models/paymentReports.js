363d4fe904b7ad95540e5440c9f773d02fe05941
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-09 08:19:58 -0300

    add

--- models/paymentReports.js
+++ models/paymentReports.js
@@ -22,38 +22,7 @@ const paymentReportsSchema = new mongoose.Schema({
     originalDueDate: Date,
     paymentDate: Date,
     clientPaymentDate: Date,
-    installmentNumber: Number,
-    invoiceUrl: String,
-    invoiceNumber: String,
-    externalReference: String,
-    deleted: Boolean,
-    anticipated: Boolean,
-    anticipable: Boolean,
-    creditDate: Date,
-    estimatedCreditDate: Date,
-    transactionReceiptUrl: String,
-    nossoNumero: String,
-    bankSlipUrl: String,
-    lastInvoiceViewedDate: Date,
-    lastBankSlipViewedDate: Date,
-    discount: {
-      value: Number,
-      limitDate: Date,
-      dueDateLimitDays: Number,
-      type: String
-    },
-    fine: {
-      value: Number,
-      type: String
-    },
-    interest: {
-      value: Number,
-      type: String
-    },
-    postalService: Boolean,
-    custody: String,
-    refunds: mongoose.Schema.Types.Mixed // Pode conter qualquer tipo de dado
-
+    
   },
 });
 const PaymentReports = mongoose.model('PaymentReports', paymentReportsSchema);
