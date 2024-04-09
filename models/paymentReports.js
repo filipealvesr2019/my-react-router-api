26d27d3dc1cbd0f150bd387d272b80fc8b5f677d
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-09 09:21:23 -0300

    add

--- models/paymentReports.js
+++ models/paymentReports.js
@@ -39,7 +39,37 @@ const paymentReportsSchema = new mongoose.Schema({
     lastInvoiceViewedDate: String,
     lastBankSlipViewedDate: String,
     postalService: String,
-    
+    creditCard:{
+      creditCardNumber: String,
+      creditCardBrand: String,
+      creditCardToken: String
+    },
+    discount:{
+      value: Number,
+      dueDateLimitDays: Number,
+      limitedDate: String,
+      type: String
+    },
+    fine:{
+      value: Number,
+      type: String
+    },
+    interest:{
+      value: Number,
+      type: String
+    },
+    split:{
+      walletId: String,
+      fixedValue: Number,
+      status: String,
+      refusalReason: String
+    },
+    chargeback:{
+      status: String,
+      reason: String
+    },
+    refunds: String,
+
   },
 });
 const PaymentReports = mongoose.model('PaymentReports', paymentReportsSchema);
