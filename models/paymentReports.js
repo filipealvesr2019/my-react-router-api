6cd3a0272942b7ec66bfc24bddbd2353fb147bcb
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-09 08:04:02 -0300

    add

--- models/paymentReports.js
+++ models/paymentReports.js
@@ -5,12 +5,54 @@ const paymentReportsSchema = new mongoose.Schema({
     id: String,
   event: String,
   payment:{
+    object: String,
     id: String,
     status: String,
     dateCreated: String,
     subscription: String,
     installment: String,
-    paymentLink: String
+    paymentLink: String,
+    value: Number,
+    netValue: Number,
+    description: String,
+    billingType: String,
+    canBePaidAfterDueDate: Boolean,
+    status: String,
+    dueDate: Date,
+    originalDueDate: Date,
+    paymentDate: Date,
+    clientPaymentDate: Date,
+    installmentNumber: Number,
+    invoiceUrl: String,
+    invoiceNumber: String,
+    externalReference: String,
+    deleted: Boolean,
+    anticipated: Boolean,
+    anticipable: Boolean,
+    creditDate: Date,
+    estimatedCreditDate: Date,
+    transactionReceiptUrl: String,
+    nossoNumero: String,
+    bankSlipUrl: String,
+    lastInvoiceViewedDate: Date,
+    lastBankSlipViewedDate: Date,
+    discount: {
+      value: Number,
+      limitDate: Date,
+      dueDateLimitDays: Number,
+      type: String
+    },
+    fine: {
+      value: Number,
+      type: String
+    },
+    interest: {
+      value: Number,
+      type: String
+    },
+    postalService: Boolean,
+    custody: String,
+    refunds: mongoose.Schema.Types.Mixed // Pode conter qualquer tipo de dado
 
   },
 });
