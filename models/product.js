c496ab46c983c15a1a9b4443a818aeb59bfb2831
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-06 09:24:43 -0300

    refactor: adiciona controller  de produtos filtrados.

--- models/product.js
+++ models/product.js
@@ -38,6 +38,10 @@ const productSchema = new mongoose.Schema({
             type: Number,
             default: 0, // Quantidade inicialmente disponível para este tamanho
           },
+          inStockSize: {
+            type: Boolean,
+            default: false,
+          },
         },
       ],
 
