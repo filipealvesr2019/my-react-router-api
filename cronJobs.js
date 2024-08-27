441ca620f64a76e78c095024c46a376fa49e3d65
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-27 09:19:33 -0300

    add

---
+++ cronJobs.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+const express = require("express");
+const Cart = require("./models/cart");
+const router = express.Router();
+// Configurar o cron job para rodar a cada 3 minutos
+cron.schedule("*/3 * * * *", async () => {
+  try {
+    console.log("Executando tarefa de limpeza de carrinhos...");
+
+    // Excluir todos os produtos dos carrinhos
+    await Cart.updateMany({}, { $set: { products: [] } });
+
+    console.log("Todos os produtos dos carrinhos foram excluídos.");
+  } catch (error) {
+    console.error("Erro ao limpar os carrinhos:", error);
+  }
+});
+
+module.exports = router;
