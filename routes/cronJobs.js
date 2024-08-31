2f149042cad820a354a32559d46b5459aa1fb964
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-31 16:02:00 -0300

    feat: adiciona logica de atualisar o estoque.

--- routes/cronJobs.js
+++ routes/cronJobs.js
@@ -20,24 +20,26 @@ cron.schedule('*/30 * * * *', async () => {
 
 
 // Função para atualizar o estoque
+// Função para atualizar o estoque
 const updateStock = async () => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
-    // Buscar todos os pedidos com status "RECEIVED"
-    const receivedOrders = await PixQRcode.find({ status: "RECEIVED" });
+    // Buscar todos os pedidos com status "RECEIVED" e que ainda não tiveram o estoque atualizado
+    const receivedOrders = await PixQRcode.find({ status: "RECEIVED", stockUpdated: false }).session(session);
 
     for (const order of receivedOrders) {
       for (const product of order.products) {
         const { productId, quantity, size, color } = product;
 
         // Encontrar o produto correspondente
-        const foundProduct = await Product.findById(productId);
+        const foundProduct = await Product.findById(productId).session(session);
         if (foundProduct) {
           // Encontrar a variação de cor e tamanho correspondentes
-          const variation = foundProduct.variations.find(
-            (v) => v.color === color
-          );
+          const variation = foundProduct.variations.find(v => v.color === color);
           if (variation) {
-            const sizeObj = variation.sizes.find((s) => s.size === size);
+            const sizeObj = variation.sizes.find(s => s.size === size);
             if (sizeObj) {
               // Subtrair a quantidade do estoque
               sizeObj.quantityAvailable -= parseInt(quantity, 10);
@@ -48,19 +50,27 @@ const updateStock = async () => {
           }
 
           // Verificar se o produto ainda está em estoque
-          foundProduct.inStock = foundProduct.variations.some((v) =>
-            v.sizes.some((s) => s.inStockSize)
-          );
+          foundProduct.inStock = foundProduct.variations.some(v => v.sizes.some(s => s.inStockSize));
 
           // Salvar as alterações do produto
-          await foundProduct.save();
+          await foundProduct.save({ session });
         }
       }
+
+      // Marcar o pedido como processado
+      order.stockUpdated = true;
+      await order.save({ session });
     }
+
+    await session.commitTransaction();
+    session.endSession();
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
     console.error("Erro ao atualizar o estoque:", error);
   }
 };
+
 
 // Executar a função `updateStock` a cada minuto
 cron.schedule("* * * * *", updateStock);
