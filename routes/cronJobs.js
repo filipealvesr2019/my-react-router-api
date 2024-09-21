5e2dd3a27152b5536b4b5ac957bf61cea94d20ce
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-21 12:40:44 -0300

    refactor: adiciona sitemap

--- routes/cronJobs.js
+++ routes/cronJobs.js
@@ -37,11 +37,7 @@ const generateSitemap = async () => {
       { url: '/', changefreq: 'daily', priority: 1.0 },
       { url: '/perfil', changefreq: 'monthly', priority: 0.8 },
       { url: '/conta', changefreq: 'monthly', priority: 0.8 },
-      ...products.map(category => ({
-        url: `/products/${encodeURIComponent(category.name.replace(/\s+/g, '-'))}/mixedProducts`,
-        changefreq: 'weekly',
-        priority: 0.6,
-      })),
+
       ...products.map(product => ({
         url: `/products/${encodeURIComponent(product.name.replace(/\s+/g, '-'))}/${product.id}`,
         changefreq: 'weekly',
@@ -74,7 +70,7 @@ cron.schedule('0 0 * * *', () => {
   generateSitemap();
 });
 
-// Agendar a execução da função a cada segundo
+// // Agendar a execução da função a cada segundo
 // cron.schedule('* * * * * *', () => {
 //   console.log('Atualizando o sitemap...');
 //   generateSitemap();
